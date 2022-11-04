83dd3abe8c76515da94f8ae6698bfa7590376a86
Juhana Kuparinen <juhku@elisanet.fi>
2022-11-04 19:28:12 +0200

    add sass, layout, bonsai page

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/bonsai">Bonsai</a>
+  <a href="/settings">Settings</a>
+</nav>
+
+<main>
+  <slot></slot>
+</main>
---
+++ client/src/routes/bonsai/+page.svelte
@@ -0,0 +1,9 @@
+
+<h1 style="font-size: 3em;">🅱️ONSAI 😂👌</h1>
+
+<style lang="scss">
+  h1 {
+    text-align: center;
+    margin-top: 2em;
+  }
+</style>
