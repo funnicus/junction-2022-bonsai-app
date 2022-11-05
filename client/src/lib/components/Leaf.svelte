acc1eb067daff6de73613ce0398ec2cc8fe4e7d4
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2022-11-05 13:43:48 +0200

    fancy anims

--- client/src/lib/components/Leaf.svelte
+++ client/src/lib/components/Leaf.svelte
@@ -7,13 +7,14 @@
 
   const rnd = random(depth.toString()).double();
 
-  const scale = 5 * (1/depth)
+  const scale = 1 + ((1/depth) * 4)
 </script>
 
 <g style="z-index: 30">
   <polygon x="0" y="0" fill="#6C9D2D" points="{generateLeafPath(scale, depth.toString())}" />
-  <polygon transform="translate(5, 5) scale(0.5) rotate({rnd*360})" fill="#6C9D2D" points="{generateLeafPath(scale, (depth + 1).toString())}" />
+  <polygon transform="translate(5, 5) scale(0.5) rotate({rnd*360})" fill="#74a339" points="{generateLeafPath(scale, (depth + 1).toString())}" />
   <polygon transform="translate(-5, -5) scale(0.75) rotate({-rnd*180})" fill="#6C9D2D" points="{generateLeafPath(scale, (depth + 1).toString())}" />
+  <polygon transform="translate(-15, 5) scale(0.5) rotate({-rnd*180})" fill="#679133" points="{generateLeafPath(scale, (depth + 1).toString())}" />
 </g>
 
 
