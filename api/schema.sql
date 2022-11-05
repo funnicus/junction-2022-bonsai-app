26faed85769a15698d304b6420e37e6293cf8cfa
Tuomas Rinne <tuomas.rin@hotmail.com>
2022-11-05 18:17:18 +0200

    fix db fields

--- api/schema.sql
+++ api/schema.sql
@@ -1,17 +1,17 @@
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, tasks;
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
   username VARCHAR(100),
   password_hash VARCHAR(100),
   data json,
-  completedTasks integer[],
-  quiz_result json
+  completed_tasks integer[],
+  quiz_results json
 );
 
 CREATE TABLE tasks (
   id serial PRIMARY KEY,
   title VARCHAR(100),
-  category VARCHAR(100)
+  category VARCHAR(100),
   description VARCHAR(100)
 )
