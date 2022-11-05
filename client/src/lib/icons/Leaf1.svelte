7697d47749ce9cde63d6d91460ec9663a532081e
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2022-11-05 18:42:58 +0200

    stuff

---
+++ client/src/lib/icons/Leaf1.svelte
@@ -0,0 +1,4 @@
+<svg width="50" height="24" viewBox="0 0 99 55" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M14.5 9.5L0 28.5L22.5 33L41.5 47.5H59.5L92 55L99 42L92 23L74.5 19.5L59.5 0.5L50.5 9.5H33.5H14.5Z" fill="#74A339"/>
+  </svg>
+  
