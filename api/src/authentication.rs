54e15b2403bf81fef39a48c22b4309074a391408
Tuomas Rinne <tuomas.rin@hotmail.com>
2022-11-05 12:02:06 +0200

    add login and authentication

---
+++ api/src/authentication.rs
@@ -0,0 +1,97 @@
+use chrono::{Duration, Utc};
+use jsonwebtoken::{
+    decode, encode, errors::ErrorKind, DecodingKey, EncodingKey, Header, Validation,
+};
+use lazy_static::lazy_static;
+use rocket::{
+    http::Status,
+    request::{FromRequest, Outcome},
+    response::status::Custom,
+};
+use serde::{Deserialize, Serialize};
+
+const BEARER: &str = "Bearer ";
+const AUTHORIZATION: &str = "Authorization";
+const SECRET: &str = "secret"; // NOTE: This would not be here in production app
+
+lazy_static! {
+    static ref TOKEN_EXPIRATION: Duration = Duration::minutes(20);
+}
+
+// Used when decoding a token to `Claims`
+#[derive(Debug, PartialEq, Eq)]
+pub enum AuthenticationError {
+    Missing,
+    Decoding(String),
+    Expired,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Claims {
+    pub name: String,
+    exp: usize,
+}
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for Claims {
+    type Error = AuthenticationError;
+
+    async fn from_request(request: &'r rocket::Request<'_>) -> Outcome<Self, Self::Error> {
+        match request.headers().get_one(AUTHORIZATION) {
+            None => Outcome::Failure((Status::Forbidden, AuthenticationError::Missing)),
+            Some(value) => match Claims::from_authorization(value) {
+                Err(e) => Outcome::Failure((Status::Forbidden, e)),
+                Ok(claims) => Outcome::Success(claims),
+            },
+        }
+    }
+}
+
+impl Claims {
+    pub fn from_name(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            exp: 0,
+        }
+    }
+
+    fn from_authorization(value: &str) -> Result<Self, AuthenticationError> {
+        let token = value.strip_prefix(BEARER).map(str::trim);
+
+        if token.is_none() {
+            return Err(AuthenticationError::Missing);
+        }
+
+        let token = token.unwrap();
+
+        let token = decode::<Claims>(
+            token,
+            &DecodingKey::from_secret(SECRET.as_ref()),
+            &Validation::default(),
+        )
+        .map_err(|e| match e.kind() {
+            ErrorKind::ExpiredSignature => AuthenticationError::Expired,
+            _ => AuthenticationError::Decoding(e.to_string()),
+        })?;
+
+        Ok(token.claims)
+    }
+
+    pub fn into_token(mut self) -> Result<String, Custom<String>> {
+        let expiration = Utc::now()
+            .checked_add_signed(*TOKEN_EXPIRATION)
+            .expect("failed to create an expiration time")
+            .timestamp();
+
+        self.exp = expiration as usize;
+
+        let token = encode(
+            &Header::default(),
+            &self,
+            &EncodingKey::from_secret(SECRET.as_ref()),
+        )
+        .map_err(|e| Custom(Status::BadRequest, e.to_string()))?;
+
+        Ok(token)
+    }
+}
--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,11 +1,17 @@
+mod authentication;
+
 #[macro_use]
 extern crate rocket;
 
-use rocket::response::status::BadRequest;
+use argon2::{
+    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+    Argon2, PasswordHash, PasswordVerifier,
+};
+use authentication::Claims;
+use rocket::response::status::{BadRequest, Unauthorized};
 use rocket::serde::json::Json;
 use rocket::State;
 use serde::{Deserialize, Serialize};
-use serde_json::json;
 use shuttle_service::error::CustomError;
 use sqlx::{Executor, FromRow, PgPool};
 
@@ -15,7 +21,21 @@ struct MyState(PgPool);
 struct User {
     pub id: i32,
     pub username: String,
-    data: serde_json::Value,
+    pub password_hash: String,
+    // data: serde_json::Value,
+}
+#[derive(Serialize)]
+struct UserResponse {
+    pub id: i32,
+    pub username: String,
+}
+impl UserResponse {
+    pub fn from_user(user: User) -> Self {
+        UserResponse {
+            id: user.id,
+            username: user.username,
+        }
+    }
 }
 #[derive(Serialize, Deserialize)]
 struct Data {
@@ -27,42 +47,105 @@ struct Data {
     length: Option<i32>,
 }
 
+#[derive(Deserialize)]
+struct LoginRequest {
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize)]
+struct LoginResponse {
+    token: String,
+}
+
 #[get("/")]
 fn index() -> &'static str {
     "Bonsai"
 }
 
+#[post("/login", data = "<login>")]
+async fn login(
+    state: &State<MyState>,
+    login: Json<LoginRequest>,
+) -> Result<Json<LoginResponse>, Unauthorized<String>> {
+    let user: User = sqlx::query_as("SELECT * FROM users WHERE username = $1")
+        .bind(&login.username)
+        .fetch_one(&state.0)
+        .await
+        .map_err(|e| Unauthorized(Some(e.to_string())))?;
+
+    let argon2 = Argon2::default();
+
+    if argon2
+        .verify_password(
+            login.password.as_bytes(),
+            &PasswordHash::new(&user.password_hash).unwrap(),
+        )
+        .is_ok()
+    {
+        let claim = Claims::from_name(&login.username);
+        let response = LoginResponse {
+            token: claim
+                .into_token()
+                .map_err(|error| Unauthorized(Some(error.1)))?,
+        };
+
+        Ok(Json(response))
+    } else {
+        Err(Unauthorized(Some("Incorrect password".to_string())))
+    }
+}
+
 #[get("/create_user")]
-async fn create_user(state: &State<MyState>) -> Result<Json<User>, BadRequest<String>> {
+async fn create_user(state: &State<MyState>) -> Result<Json<UserResponse>, BadRequest<String>> {
+    let salt = SaltString::generate(&mut OsRng);
+
+    // Argon2 with default params (Argon2id v19)
+    let argon2 = Argon2::default();
+
+    // Hash password to PHC string ($argon2id$v=19$...)
+    let password_hash = argon2
+        .hash_password(b"test", &salt)
+        .map_err(|err| BadRequest(Some(err.to_string())))?
+        .to_string();
+
     let user = Json(User {
         id: 1,
         username: "test".to_string(),
-        data: json!(Data {
+        password_hash,
+        /*   data: json!(Data {
             r#type: "root".to_string(),
             time: 1231212321,
             children: vec![],
             taskId: 1.to_string(),
             angle: None,
             length: None,
-        }),
+        }),*/
     });
-    let user = sqlx::query_as("INSERT INTO users(username) VALUES ($1) RETURNING id, username")
-        .bind(&user.username)
-        .fetch_one(&state.0)
-        .await
-        .map_err(|e| BadRequest(Some(e.to_string())))?;
+    let user:User = sqlx::query_as(
+        "INSERT INTO users(username, password_hash) VALUES ($1,$2) RETURNING id, username, password_hash",
+    )
+    .bind(&user.username)
+    .bind(&user.password_hash)
+    .fetch_one(&state.0)
+    .await
+    .map_err(|e| BadRequest(Some(e.to_string())))?;
 
-    Ok(Json(user))
+    Ok(Json(UserResponse::from_user(user)))
 }
-#[get("/users/<id>")]
-async fn get_user(state: &State<MyState>, id: &str) -> Result<Json<User>, BadRequest<String>> {
-    let user = sqlx::query_as("SELECT * FROM users WHERE id = $1")
-        .bind(id.parse::<i32>().unwrap())
+
+#[get("/user")]
+async fn get_user(
+    state: &State<MyState>,
+    claims: Claims,
+) -> Result<Json<UserResponse>, BadRequest<String>> {
+    let user = sqlx::query_as("SELECT * FROM users WHERE username = $1")
+        .bind(claims.name)
         .fetch_one(&state.0)
         .await
         .map_err(|e| BadRequest(Some(e.to_string())))?;
 
-    Ok(Json(user))
+    Ok(Json(UserResponse::from_user(user)))
 }
 
 #[shuttle_service::main]
@@ -75,5 +158,5 @@ async fn rocket(#[shuttle_shared_db::Postgres] pool: PgPool) -> shuttle_service:
 
     Ok(rocket::build()
         .manage(state)
-        .mount("/", routes![index, create_user, get_user]))
+        .mount("/", routes![index, create_user, get_user, login]))
 }
