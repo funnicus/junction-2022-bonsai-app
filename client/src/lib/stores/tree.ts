d7400fd6ae1458279be0bda1af625eeb0813097b
Matias Kumpulainen <kumpulainen.matias@gmail.com>
2022-11-05 12:37:34 +0200

    refactor

--- client/src/lib/stores/tree.ts
+++ client/src/lib/stores/tree.ts
@@ -1,1 +1,75 @@
-export const createTreeStore = () => {}
+import type { Data } from "$lib/dataSchema"
+import { writable, type Readable } from "svelte/store"
+
+type TreeState = {selectedNode: Data | null, nodes: Data[]}
+
+export type TreeStore = Readable<TreeState> & {
+  addLeaf: () => void
+  addExtension: (angle: number, length: number) => void
+  setSelectedNode: (node: Data | null) => void
+}
+
+
+
+export const createTreeStore = (): TreeStore => {
+  const state = writable<TreeState>({
+    selectedNode: null,
+    nodes:[
+    {
+      type:'extension',
+      length: 20,
+      angle: 5,
+      children: [],
+      taskId: "",
+      time: Date.now()
+    }
+  ]})
+
+  const addLeaf = () => {
+    const item: Data =  {
+      type:'leaf',
+      children: [],
+      taskId: "123981628362",
+      time: Date.now()
+    }
+
+    state.update(prev => {
+      if (!prev.selectedNode) return prev
+      prev.selectedNode?.children.push(item)
+      prev.selectedNode = null // clear selection
+      return prev
+    })
+  }
+
+  const addExtension = (angle: number, length: number) => {
+    const item: Data =  {
+      type:'extension',
+      length,
+      angle,
+      children: [],
+      taskId: "123981628362",
+      time: Date.now()
+    }
+
+    state.update(prev => {
+      if (!prev.selectedNode) return prev
+      prev.selectedNode?.children.push(item)
+      prev.selectedNode = item // select newly created item
+      return prev
+    })
+  }
+
+  const setSelectedNode = (node: Data | null) => {
+    state.update(prev => {
+      prev.selectedNode = node;
+      return prev
+    })
+  }
+
+  return {
+    subscribe: state.subscribe,
+    addLeaf,
+    addExtension,
+    setSelectedNode,
+  }
+}
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,55 +1,14 @@
 <script lang="ts">
 	import Tree from "$lib/components/Tree.svelte";
 	import type { Data } from "$lib/dataSchema";
+	import { createTreeStore } from "$lib/stores/tree";
 
-  let testData: Data[] = [
-    {
-      type:'extension',
-      length: 20,
-      angle: 5,
-      children: [],
-      taskId: "",
-      time: Date.now()
-    }
-  ]
-  let selectedNode: Data | null = null
+  let tree = createTreeStore()
 
   let angle = 0
   let length = 50
 
-  const addLeaf = () => {
-    const item: Data =  {
-      type:'leaf',
-      children: [],
-      taskId: "123981628362",
-      time: Date.now()
-    }
-
-    if (!selectedNode) return
-
-    selectedNode.children.push(item)
-    selectedNode = null
-    testData = testData // tell svelte to update tree
-  }
-
-  const addExtension = () => {
-    const item: Data =  {
-      type:'extension',
-      length,
-      angle,
-      children: [],
-      taskId: "123981628362",
-      time: Date.now()
-    }
-
-    if (!selectedNode) return
-
-    selectedNode?.children.push(item)
-    selectedNode = null
-    testData = testData // tell svelte to update tree
-  }
-
-  $:console.log(testData)
+  $:console.log(tree)
 
 </script>
 <h1>Welcome to SvelteKit</h1>
@@ -59,9 +18,9 @@
 
 <div>
   <Tree
-    nodes={testData}
-    on:select={e => selectedNode = e.detail}
-    {selectedNode}
+    nodes={$tree.nodes}
+    on:select={e => tree.setSelectedNode(e.detail)}
+    selectedNode={$tree.selectedNode}
     {angle}
     {length}
   />
@@ -77,8 +36,8 @@
   <input type="range" min="20" max="75" bind:value={length} />
 </label>
 
-<button on:click={addLeaf}>add leaf</button>
-<button on:click={addExtension}>add extension</button>
+<button on:click={tree.addLeaf}>add leaf</button>
+<button on:click={() => tree.addExtension(angle, length)}>add extension</button>
 
 <style>
   label {
